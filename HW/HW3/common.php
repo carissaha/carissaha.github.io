d0077960dbad84278c1706e2ed99c401cb0d7efb
carissaha <carissathalim@gmail.com>
2025-03-11 13:30:34 -0400

    working on hw3

---
+++ HW/HW3/common.php
@@ -0,0 +1,13 @@
+<?php
+
+function footer() {
+    echo <<<HTML
+        <div id="w3c">
+            <p>
+                <a href="https://validator.w3.org/">HTML Validator</a> |
+                <a href="https://jigsaw.w3.org/css-validator/">CSS Validator</a>
+            </p>
+        </div>
+HTML;
+}
+?>
--- HW/HW3/loginScripts/index.php
+++ HW/HW3/loginScripts/index.php
@@ -4,6 +4,34 @@ if(!isset($_SESSION['UserData']['Username'])){
 	header("location:login.php");
 	exit;
 }
+
+include 'common.php';
 ?>
 
-Congratulation! You have logged into password protected page. <a href="logout.php">Click here</a> to Logout.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NerdLuv - Where Meek Geeks Meet</title>
+</head>
+<body>
+	<div>
+	<h1>nerdLuv™</h1>
+	<p>where meek geeks meet</p>
+
+    <p>Welcome!</p>
+    <p><a href="">Sign up for a new account</a></p>
+	<p><a href="matches.php">Check your matches</a></p>
+    
+	<p>This page is for single nerds to meet and date each other! Type in
+		your personal information and wait for the nerdly luv to begin!
+		Thank you for using our site.</p>
+	<p>Results and page (C) Copyright NerdLuv Inc.</p>
+	<p><a href="login.php">Back to login</p>
+	<p><a href="logout.php">Logout</p>
+	</div>
+
+	<?php footer(); // Function from common.php ?>
+</body>
+</html>
